347dd09931cd5b247ac1913fb77ea886bde363e9
hourglasshoro <hourglasshoro.628@gmail.com>
2023-06-24 04:28:38 -0400

    add lit encryption

--- src/components/sismoConnect.tsx
+++ src/components/sismoConnect.tsx
@@ -2,6 +2,7 @@
 
 import { sismoVerify } from '@/actions/sismoVerify'
 import { sismoAppId } from '@/constants/sismo'
+import { litChain } from '@/constants/lit'
 import {
   AuthType,
   SismoConnectButton,
@@ -11,6 +12,12 @@ import {
 } from '@sismo-core/sismo-connect-react'
 
 import { FC, useState } from 'react'
+import {
+  checkAndSignAuthMessage,
+  decryptString,
+  encryptString,
+  uint8arrayToString
+} from '@lit-protocol/lit-node-client'
 
 type Props = {}
 
@@ -45,19 +52,96 @@ export const SismoConnect: FC<Props> = () => {
     }
   }
 
+  const handleSaveProofs = async () => {
+    setLoading(true)
+    setError(null)
+    try {
+      if (!!proofs) {
+        const litNodeClient = new LitNodeClient()
+        await litNodeClient.connect()
+
+        const stringToEncrypt = JSON.stringify(proofs)
+
+        const authSig = await checkAndSignAuthMessage({
+          chain: litChain
+        })
+
+        const { encryptedString, symmetricKey } = await encryptString(
+          stringToEncrypt
+        )
+
+        console.log({
+          symmetricKey,
+          authSig,
+          litChain
+        })
+
+        const encryptedSymmetricKey: Uint8Array =
+          await litNodeClient.saveEncryptionKey({
+            accessControlConditions,
+            symmetricKey,
+            authSig,
+            chain: litChain
+          })
+
+        // TODO: save encryptedSymmetricKey to ipfs
+
+        // Decrypto
+        const fetchedSymmetricKey = await litNodeClient.getEncryptionKey({
+          accessControlConditions,
+          toDecrypt: uint8arrayToString(encryptedSymmetricKey, 'base16'),
+          chain: litChain,
+          authSig
+        })
+
+        const decryptedString = await decryptString(
+          encryptedString,
+          fetchedSymmetricKey
+        )
+
+        console.log('decryptedString', decryptedString)
+      } else {
+        console.error('the proofs is empty')
+      }
+    } catch (e) {
+      setError(error)
+      console.error(e)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const accessControlConditions = [
+    {
+      contractAddress: '',
+      standardContractType: '',
+      chain: litChain,
+      method: 'eth_getBalance',
+      parameters: [':userAddress', 'latest'],
+      returnValueTest: {
+        comparator: '>=',
+        value: '1000000000000'
+      }
+    }
+  ]
+
   return (
     <div>
       {!!proofs ? (
-        <ul>
-          {proofs.map(
-            (p) =>
-              p.auths && (
-                <li key={p.auths[0].userId}>
-                  <p>{p.auths[0].userId}</p>
-                </li>
-              )
-          )}
-        </ul>
+        <div>
+          <ul>
+            {proofs.map(
+              (p) =>
+                p.auths && (
+                  <li key={p.auths[0].userId}>
+                    <p>{p.auths[0].userId}</p>
+                  </li>
+                )
+            )}
+          </ul>
+          <button onClick={handleSaveProofs}>Save to ceramic</button>
+          {loading && <div>Loading...</div>}
+        </div>
       ) : (
         <SismoConnectButton
           config={sismoConnectConfig}
